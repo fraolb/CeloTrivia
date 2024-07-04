168425646fac0549e8b016c289ab4746e06590bf
fraolb <fraolbereket@gmail.com>
2024-07-05 00:19:05 +0300

    added timer and answer question to the pages

--- packages/react-app/pages/host/[room].tsx
+++ packages/react-app/pages/host/[room].tsx
@@ -2,6 +2,18 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import io from "socket.io-client";
 
+interface Question {
+  text: string;
+  choices: string[];
+  answer: string;
+}
+
+interface UserAnswer {
+  answer: string;
+  id: string;
+  point: number;
+}
+
 const socket = io("http://localhost:3001", {
   auth: { isCreator: true },
 });
@@ -10,15 +22,18 @@ const HostPage = () => {
   const router = useRouter();
   const { room } = router.query;
 
-  const [message, setMessage] = useState("");
-  const [messageReceived, setMessageReceived] = useState("");
-  const [quizStarted, setQuizStarted] = useState(false);
-  const [question, setQuestion] = useState({
+  const [message, setMessage] = useState<string>("");
+  const [messageReceived, setMessageReceived] = useState<string>("");
+  const [quizStarted, setQuizStarted] = useState<boolean>(false);
+  const [question, setQuestion] = useState<Question>({
     text: "What is the capital of France?",
     choices: ["Paris", "Berlin", "Madrid", "Rome"],
     answer: "Paris",
   });
-  const [showAnswer, setShowAnswer] = useState(false);
+  const [showAnswer, setShowAnswer] = useState<boolean>(false);
+  const [userAnswers, setUserAnswers] = useState<UserAnswer[]>([]);
+  const [correctUsers, setCorrectUsers] = useState<string[]>([]);
+  const [countdown, setCountdown] = useState<number>(0);
 
   const sendMessage = () => {
     socket.emit("send_message", { message, room });
@@ -26,14 +41,43 @@ const HostPage = () => {
 
   const startQuiz = () => {
     socket.emit("start_quiz", room);
-    setTimeout(() => {
-      setQuizStarted(true);
-      socket.emit("send_question", { room, question });
-      setTimeout(() => {
+    let startCountdown = 5;
+    setCountdown(startCountdown);
+
+    const countdownInterval = setInterval(() => {
+      startCountdown -= 1;
+      setCountdown(startCountdown);
+      if (startCountdown <= 0) {
+        clearInterval(countdownInterval);
+        setQuizStarted(true);
+        startQuestionTimer();
+      }
+    }, 1000);
+  };
+
+  const startQuestionTimer = () => {
+    let questionCountdown = 10;
+    setCountdown(questionCountdown);
+
+    socket.emit("send_question", { room, question });
+
+    const questionInterval = setInterval(() => {
+      questionCountdown -= 1;
+      setCountdown(questionCountdown);
+      if (questionCountdown <= 0) {
+        clearInterval(questionInterval);
         setShowAnswer(true);
         socket.emit("show_answer", { room, answer: question.answer });
-      }, 20000); // Show answer after 20 seconds
-    }, 5000); // Start quiz after 5 seconds countdown
+        checkAnswers();
+      }
+    }, 1000);
+  };
+
+  const checkAnswers = () => {
+    const correctUsers = userAnswers
+      .filter((userAnswer) => userAnswer.answer === question.answer)
+      .map((userAnswer) => userAnswer.id);
+    setCorrectUsers(correctUsers);
   };
 
   useEffect(() => {
@@ -48,8 +92,18 @@ const HostPage = () => {
       socket.on("receive_message", (data) => {
         setMessageReceived(data.message);
       });
+      socket.on("receive_users_answer", (data: UserAnswer) => {
+        const point = data.answer === question.answer ? 1 : 0;
+        const usrData = { ...data, point };
+        setUserAnswers((prevUserAnswers) => [...prevUserAnswers, usrData]);
+      });
     }
-  }, [socket]);
+
+    return () => {
+      socket.off("receive_message");
+      socket.off("receive_users_answer");
+    };
+  }, [room]);
 
   return (
     <div className="flex flex-col items-center min-h-screen py-2 bg-gray-100">
@@ -72,7 +126,7 @@ const HostPage = () => {
         <h1>Message:</h1>
         {messageReceived}
       </div>
-      {!quizStarted ? (
+      {!quizStarted && countdown === 0 ? (
         <button
           onClick={startQuiz}
           className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 mt-4"
@@ -81,15 +135,37 @@ const HostPage = () => {
         </button>
       ) : (
         <div>
-          <h2 className="text-2xl mt-4">{question.text}</h2>
-          <ul>
-            {question.choices.map((choice, index) => (
-              <li key={index}>{choice}</li>
-            ))}
-          </ul>
-          {showAnswer && (
-            <div className="mt-4">
-              <h3 className="text-xl">Answer: {question.answer}</h3>
+          {countdown > 0 && (
+            <h2 className="text-2xl mt-4">Starting in: {countdown}</h2>
+          )}
+          {quizStarted && (
+            <div>
+              <h2 className="text-2xl mt-4">{question.text}</h2>
+              <ul>
+                {question.choices.map((choice, index) => (
+                  <li key={index}>{choice}</li>
+                ))}
+              </ul>
+              <h2 className="text-2xl mt-4">Time remaining: {countdown}</h2>
+              {showAnswer && (
+                <div className="mt-4">
+                  <h3 className="text-xl">Answer: {question.answer}</h3>
+                  <h3 className="text-xl mt-4">
+                    Users who answered correctly:
+                  </h3>
+                  <ul>
+                    {userAnswers.length > 0 ? (
+                      userAnswers.map((user, index) => (
+                        <li key={index}>
+                          {user.id}, Pt - {user.point}
+                        </li>
+                      ))
+                    ) : (
+                      <li>No one answered.</li>
+                    )}
+                  </ul>
+                </div>
+              )}
             </div>
           )}
         </div>
--- packages/react-app/pages/join/[room].tsx
+++ packages/react-app/pages/join/[room].tsx
@@ -16,9 +16,10 @@ function Room() {
   const [message, setMessage] = useState<string>("");
   const [messageReceived, setMessageReceived] = useState<string[]>([]);
   const [question, setQuestion] = useState<Question | null>(null);
-  const [answer, setAnswer] = useState("");
-  const [yourAnswer, setYourAnswer] = useState("");
-  const [clickable, setClickable] = useState(false);
+  const [answer, setAnswer] = useState<string>("");
+  const [yourAnswer, setYourAnswer] = useState<string>("");
+  const [clickable, setClickable] = useState<boolean>(false);
+  const [countdown, setCountdown] = useState<number>(0);
 
   const sendMessage = () => {
     socket.emit("send_message", { message, room });
@@ -26,7 +27,7 @@ function Room() {
 
   const submitAnswer = (choice: string) => {
     setYourAnswer(choice);
-    //socket.emit("submit_answer", { room, answer: choice });
+    socket.emit("answer_question", { room, answer: choice });
     setClickable(false);
   };
 
@@ -35,14 +36,19 @@ function Room() {
       setMessageReceived((prevMessages) => [...prevMessages, data.message]);
     });
 
-    socket.on("receive_question", (question) => {
+    socket.on("receive_question", (question: Question) => {
       setQuestion(question);
       setAnswer(""); // Clear previous answer
       setYourAnswer("");
       setClickable(true);
+      startQuestionTimer();
     });
 
-    socket.on("receive_answer", (answer) => {
+    socket.on("quiz_started", () => {
+      startQuizTimer();
+    });
+
+    socket.on("receive_answer", (answer: string) => {
       setAnswer(answer);
     });
 
@@ -50,6 +56,7 @@ function Room() {
       socket.off("receive_message");
       socket.off("receive_question");
       socket.off("receive_answer");
+      socket.off("quiz_started");
     };
   }, []);
 
@@ -58,6 +65,32 @@ function Room() {
       socket.emit("join_room", room);
     }
   }, [room]);
+
+  const startQuizTimer = () => {
+    let startCountdown = 5;
+    setCountdown(startCountdown);
+
+    const countdownInterval = setInterval(() => {
+      startCountdown -= 1;
+      setCountdown(startCountdown);
+      if (startCountdown <= 0) {
+        clearInterval(countdownInterval);
+      }
+    }, 1000);
+  };
+
+  const startQuestionTimer = () => {
+    let questionCountdown = 10;
+    setCountdown(questionCountdown);
+
+    const questionInterval = setInterval(() => {
+      questionCountdown -= 1;
+      setCountdown(questionCountdown);
+      if (questionCountdown <= 0) {
+        clearInterval(questionInterval);
+      }
+    }, 1000);
+  };
 
   return (
     <div className="App">
@@ -74,7 +107,7 @@ function Room() {
       {messageReceived.map((msg, index) => (
         <div key={index}>{msg}</div>
       ))}
-      {answer == "" && question && (
+      {answer === "" && question && (
         <div>
           <h2 className="w-full p-2 mb-4 border border-gray-300 rounded-md text-center text-2xl">
             {question.text}
@@ -83,6 +116,7 @@ function Room() {
             <div className="w-full grid grid-cols-2 sm:grid-cols-2 gap-4">
               {question.choices.map((choice, index) => (
                 <button
+                  key={index}
                   disabled={!clickable}
                   onClick={() => submitAnswer(choice)}
                   className="w-full p-2 border border-gray-300 rounded-md text-2xl hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 hover:text-white"
@@ -92,9 +126,11 @@ function Room() {
               ))}
             </div>
           </ul>
-          {answer && <h3>Answer: {answer}</h3>}
         </div>
       )}
+      <div className="w-full p-2 mb-4 text-center text-2xl">
+        {countdown > 0 && <p>Time remaining: {countdown}</p>}
+      </div>
       <div>
         {answer !== "" && yourAnswer !== answer ? (
           <div className="w-full p-2 mb-4 text-center text-red-500 text-2xl">
