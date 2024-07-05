8f19f0dd4509b9160bd0e88b884bc723fe5599b4
fraolb <fraolbereket@gmail.com>
2024-07-05 14:30:28 +0300

    added notification while creating and restrications

--- packages/react-app/pages/create/index.tsx
+++ packages/react-app/pages/create/index.tsx
@@ -11,6 +11,11 @@ interface quizQuestionInterface {
   correctAnswer: number | null;
 }
 
+interface notificationInterfact {
+  message: string;
+  type: string;
+}
+
 const CreateTrivia: React.FC = () => {
   const router = useRouter();
   const [triviaName, setTriviaName] = useState<string>("");
@@ -18,6 +23,9 @@ const CreateTrivia: React.FC = () => {
   const [quizQuestions, setQuizQuestions] = useState<quizQuestionInterface[]>(
     []
   );
+  const [notification, setNotification] =
+    useState<notificationInterfact | null>();
+  const [loading, setLoading] = useState(false);
 
   const addQuestion = () => {
     setQuestions([...questions, questions.length]);
@@ -43,9 +51,25 @@ const CreateTrivia: React.FC = () => {
 
   const handleSave = () => {
     // Save logic goes here
+    setLoading(true);
     console.log({ triviaName, quizQuestions });
-    alert("Trivia saved!");
-    router.push(`/dashboard`);
+
+    if (triviaName == "" || quizQuestions.length == 0) {
+      setNotification({
+        message: "Add Trivia name and Questions",
+        type: "error",
+      });
+      setLoading(false);
+    } else {
+      setNotification({
+        message: "Questions are saved!",
+        type: "success",
+      });
+      setLoading(false);
+    }
+    // alert("Trivia saved!");
+    // router.push(`/dashboard`);
+    setTimeout(() => setNotification(null), 3000);
   };
 
   return (
@@ -59,6 +83,15 @@ const CreateTrivia: React.FC = () => {
       </Head>
 
       <main className="flex flex-col items-center w-full flex-1 px-4 sm:px-20 text-center">
+        {notification && (
+          <div
+            className={`fixed top-0 left-1/2 transform -translate-x-1/2 mt-12 p-2 px-4 w-3/4 rounded shadow-lg z-10 ${
+              notification.type === "success" ? "bg-green-500" : "bg-red-500"
+            } text-white`}
+          >
+            {notification.message}
+          </div>
+        )}
         <h1 className="text-4xl font-bold mt-20 mb-12 text-white">
           Create Trivia
         </h1>
@@ -67,12 +100,14 @@ const CreateTrivia: React.FC = () => {
           <button
             className="flex items-center justify-center px-2 py-2 text-lg font-semibold text-white bg-red-500 rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500"
             onClick={handleDiscard}
+            disabled={loading}
           >
             <FaTrash className="mr-2" /> Discard
           </button>
           <button
             className="flex items-center justify-center px-4 py-2 text-lg font-semibold text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
             onClick={handleSave}
+            disabled={loading}
           >
             <FaSave className="mr-2" /> Save
           </button>
@@ -85,7 +120,7 @@ const CreateTrivia: React.FC = () => {
             value={triviaName}
             onChange={(e) => setTriviaName(e.target.value)}
             required
-            className="w-full p-3 mb-4 border border-gray-300 rounded-md"
+            className="w-full p-3 mb-4 border border-gray-300 rounded-md text-center"
           />
         </form>
 
@@ -103,6 +138,7 @@ const CreateTrivia: React.FC = () => {
         <button
           className="fixed bottom-10 p-3 text-lg font-semibold text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
           onClick={addQuestion}
+          disabled={loading}
         >
           Add Question
         </button>
