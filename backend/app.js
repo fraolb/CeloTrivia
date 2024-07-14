21d6efb9d5d6dfa8a462100536885188011cbcc6
fraolb <fraolbereket@gmail.com>
2024-07-14 12:15:31 +0300

    removed origin from cors

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,6 @@ const port = process.env.PORT || 4000;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://celo-trivia.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
